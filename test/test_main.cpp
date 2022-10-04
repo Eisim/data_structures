94ce09b8df787e1455e5e6d551252cdd593f1db1
Eisim <koctik.w@mail.ru>
2022-10-04 14:09:31 +0300

    first commit

---
+++ test/test_main.cpp
@@ -0,0 +1,11 @@
+#include <gtest.h>
+//my changes
+//eto ochen' vazhnii komentarii
+
+
+
+int main(int argc, char **argv) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
